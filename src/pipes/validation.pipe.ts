62aec9f66e089dc505a396d7f7b7261dd2a742f4
Anton <a.chertok@akatech.com.ua>
2023-08-23 22:25:59 +0300

    Update validation.pipe.ts

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -13,10 +13,12 @@ export class ValidationPipe implements PipeTransform<any> {
     const object = plainToInstance(metatype, value);
     const errors = await validate(object);
     if (errors.length > 0) {
-      //   console.log(errors);
-      // validation message -> throw validation exception later
-      //   const message = errors.map(({ constraints }) => constraints[]);
-      throw new BadRequestException('Validation failed');
+      console.log(errors);
+      const message = errors.map(
+        (err) =>
+          `${err.property}: ${Object.values(err.constraints).join(', ')}`,
+      );
+      throw new BadRequestException(message);
     }
     return value;
   }
