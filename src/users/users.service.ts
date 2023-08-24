33da0260bb70e11251cd685a2dbd00b0b1c7db26
Anton <a.chertok@akatech.com.ua>
2023-08-24 17:26:22 +0300

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,7 +9,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { Sequelize } from 'sequelize-typescript';
 import { GetUserByRoleDto } from './dto/get-user-by-role';
-import { Op } from 'sequelize';
+import { Op, Transaction } from 'sequelize';
 import { CommonException } from 'src/exceptions/common.exception';
 import { UpdateUserDto } from './dto/update-user-dto';
 
@@ -70,6 +70,39 @@ export class UsersService {
           throw new NotFoundException('User not found');
         }
 
+        // Check for username and email
+        // let existingUser: User | null = null;
+        const existingUser = await this.checkIsUserExistedByUsernameAndEmail(
+          id,
+          dto,
+          t,
+        );
+
+        if (existingUser) {
+          throw new ConflictException('Username or email already exists');
+        }
+        // Check for username and email
+        // const whereConditions = [];
+        // if (dto.username) {
+        //   whereConditions.push({ username: dto.username });
+        // }
+
+        // if (dto.email) {
+        //   whereConditions.push({ email: dto.email });
+        // }
+
+        // const existingUser = this.userModel.findOne({
+        //   where: {
+        //     [Op.or]: whereConditions,
+        //   },
+        //   transaction: t,
+        // });
+
+        // if (existingUser && existingUser.id !== id) {
+        //   console.log('!!!!!!!!!!!!!!');
+        //   throw new ConflictException('Username or email are already existed');
+        // }
+
         await user.profile.update({ ...dto }, { transaction: t });
         const updatedUser = await user.update({ ...dto }, { transaction: t });
 
@@ -104,5 +137,36 @@ export class UsersService {
     } catch (error) {
       throw new CommonException(error.message, error.status);
     }
+  }
+
+  private async checkIsUserExistedByUsernameAndEmail(
+    updatingUserId: number,
+    dto: UpdateUserDto,
+    transaction?: Transaction,
+  ): Promise<User | null> {
+    let existingUser: User | null = null;
+
+    const whereConditions = [];
+    if (dto.username) {
+      whereConditions.push({ username: dto.username });
+    }
+
+    if (dto.email) {
+      whereConditions.push({ email: dto.email });
+    }
+
+    if (whereConditions.length > 0) {
+      existingUser = await this.userModel.findOne({
+        where: {
+          [Op.and]: [
+            { id: { [Op.not]: updatingUserId } },
+            { [Op.or]: whereConditions },
+          ],
+        },
+        transaction,
+      });
+    }
+
+    return existingUser;
   }
 }
